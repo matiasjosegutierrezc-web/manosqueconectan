a4d887e933b4dbfcfe876acfb6df4b161eafd254
Matías <matiasjosegutierrezc@gmail.com>
2026-05-24 09:52:29 -0300

    fix: navbar py reducido a py-2

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -37,7 +37,7 @@ export default function Nav() {
 
   return (
     <nav
-      className="fixed top-0 left-0 right-0 z-50 py-3 px-6 lg:px-12"
+      className="fixed top-0 left-0 right-0 z-50 py-2 px-6 lg:px-12"
       style={{ backgroundColor: '#6EA0BE' }}
     >
       <div className="max-w-[1200px] mx-auto flex items-center justify-between">
