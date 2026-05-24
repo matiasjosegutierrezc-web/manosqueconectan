741b16d01d7ae3f9cad02497b4f23472bfa07af2
Matías <matiasjosegutierrezc@gmail.com>
2026-05-24 10:02:31 -0300

    fix: nav altura real y MisionVision scroll ajustado

--- components/MisionVision.tsx
+++ components/MisionVision.tsx
@@ -2,7 +2,7 @@ import Reveal from '@/components/Reveal'
 
 export default function MisionVision() {
   return (
-    <section id="quienes-somos" className="bg-off-white min-h-[500px]" style={{ scrollMarginTop: '100px' }}>
+    <section id="quienes-somos" className="bg-off-white min-h-[600px]" style={{ scrollMarginTop: '60px' }}>
       <div className="grid grid-cols-1 md:grid-cols-2 items-stretch">
 
         {/* Misión */}
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -37,12 +37,12 @@ export default function Nav() {
 
   return (
     <nav
-      className="fixed top-0 left-0 right-0 z-50 py-1 px-6 lg:px-12"
+      className="fixed top-0 left-0 right-0 z-50 py-0 flex items-center px-6 lg:px-12"
       style={{ backgroundColor: '#6EA0BE' }}
     >
       <div className="max-w-[1200px] mx-auto flex items-center justify-between">
         <a href="#hero">
-          <Image src="/logo.png" alt="Manos que Conectan" width={140} height={50} className="object-contain object-left w-[90px] lg:w-[140px] h-auto" />
+          <Image src="/logo.png" alt="Manos que Conectan" width={140} height={35} className="object-contain object-left w-[90px] lg:w-[140px] h-[35px]" />
         </a>
 
         {/* Desktop links */}
